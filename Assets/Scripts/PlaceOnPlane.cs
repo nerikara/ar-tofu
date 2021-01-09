92d1967671b2487d72e6b0464a131f2763595909
nerikara <nerikara4chimi@gmail.com>
2021-01-09 21:59:37 +0900

    tofuスクリプト追加

---
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField, Tooltip("AR空間に召喚する豆腐")] GameObject tohu;
+
+    private GameObject spawnedObject;
+    private ARRaycastManager raycastManager;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            if (raycastManager.Raycast(touchPosition, hits, TrackableType.Planes))
+            {
+                // Raycastの衝突情報は距離によってソートされるため、0番目が最も近い場所でヒットした情報となります
+                var hitPose = hits[0].pose;
+
+                if (spawnedObject)
+                {
+                    spawnedObject.transform.position = hitPose.position;
+                }
+                else
+                {
+                    spawnedObject = Instantiate(tohu, hitPose.position, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
